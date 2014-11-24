d0d3bf3d496749c9ebc66f1c03f8ac75a829da3f
phillyger <ger@brilliantage.com>
2014-11-24 13:20:12 -0500

    Initial Commit

---
+++ MDRegistrationObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MDRegistrationObjC
+//
+//  Created by GER OSULLIVAN on 11/24/14.
+//  Copyright (c) 2014 brilliantage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
